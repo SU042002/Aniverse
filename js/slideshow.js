2ebf2d9d647e031fb551ae886f96441a92b29697
Sameer Uddin <su042002@outlook.com>
2022-04-16 17:37:57 +0100

    Update Slideshow with Buttons
    Removed Bloat code.

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,20 +6,13 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
     let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-    }
+
     slides[slideIndex-1].style.display = "block";
 }
