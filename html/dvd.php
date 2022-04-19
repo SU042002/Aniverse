664c9d1210555b6799d6937c9193f13a00d583fb
Sameer Uddin <su042002@outlook.com>
2022-04-19 01:23:08 +0100

    Added banner with images
    Added banners with images, with a transparent black overlay.

--- html/dvd.php
+++ html/dvd.php
@@ -4,25 +4,15 @@
     <meta charset="UTF-8">
     <title>Aniverse</title>
     <link rel="stylesheet" href="../css/default.css">
-    <link rel="stylesheet" href="../css/waves.css">
+    <link rel="stylesheet" href="../css/banner.css">
 </head>
 <body>
 
 <?php require "header.php"; ?>
 
-<div class="wave_top_orange">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-    </svg>
-</div>
-<div class="banner_salmon">
+<div class="banner" id="dvd">
     <h1>DVD</h1>
     <p>Welcome to your new and improved one stop resource for a massive range of Anime on DVD and Blu-Ray.</p>
-</div>
-<div class="wave_bottom_orange">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-    </svg>
 </div>
 
 <?php require "footer.php"; ?>
