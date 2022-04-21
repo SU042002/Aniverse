e54988a6e09fb70e848f5bddc356ecbfc221ef8d
Sameer Uddin <su042002@outlook.com>
2022-04-21 02:04:55 +0100

    Product displayed on index
    All the products in the database are displayed on the website. No CSS has been applied but the code is functioning.

--- html/admin.php
+++ html/admin.php
@@ -11,31 +11,31 @@
 <?php require "header.php"; ?>
 
 <div class="product_container" id="all_products">
-    <form name="addProd" action="../php/product_submission.php" method="post" enctype="multipart/form-data" class="addForm"> <!-- created a form -->
+    <form name="addProd" action="../php/product_submission.php" method="post" enctype="multipart/form-data"
+          class="addForm"> <!-- created a form -->
         <h1>Add Product</h1> <!-- header so the user knows they can add product -->
         <!-- enctype is needed because there are files that are uploaded, images -->
+        <p>All the fields are required to submit!</p>
         <div class="prodInput">
-            <input type="text" name="pnm" placeholder="Product Name" required autocomplete="off">
+            <input type="text" name="productName" placeholder="Product Name" required autocomplete="off">
         </div> <!-- creating seperate divisions for all the inputs -->
         <!-- auto complete is switched off for everything so history isn't saved -->
         <!-- required tag here so that nothing is left empty before data is sent off -->
         <div class="prodInput">
-            <input type="text" name="pprice" placeholder="Price" required autocomplete="off">
+            <input type="text" name="productPrice" placeholder="Price" required autocomplete="off">
         </div> <!-- input for price -->
-
         <div class="prodInput">
-            <input type="text" name="pdesc" placeholder="Product Description" autocomplete="off" required>
+            <textarea name="productDesc" rows="4" cols="0" autocomplete="off" placeholder="Description" required></textarea>
         </div> <!-- so that the user can write product description -->
-
         <div class="prodImage">
             <label>
                 Upload Image
-                <input type="file" name="pimage" placeholder="Add Image" required autocomplete="off" style="visibility: hidden;">
+                <input type="file" name="productImg" placeholder="Add Image" required autocomplete="off"
+                       style="visibility: hidden;">
             </label> <!-- label used so it can be customized in css -->
         </div>
-
         <div class="prodInput">
-            <select name="pcategory" required>
+            <select name="productCat" required>
                 <option value="DVD">DVD</option>
                 <option value="Manga">Manga</option>
                 <option value="Figures">Figures</option>
@@ -45,13 +45,13 @@
       different categories -->
         </div>
 
-        <div class="prodInput">
-            <button type="submit" class="add-button" name="submit1">Add Product</button>
+        <div class="prodInput" id="submit_button">
+            <button type="submit" class="add-button" name="submission">Add Product</button>
         </div> <!-- when the user presses the button they will be able to submit the information -->
 
     </form>
 
-<?php require "footer.php"; ?>
+    <?php require "footer.php"; ?>
 
 </body>
 </html>
--- html/index.php
+++ html/index.php
@@ -6,11 +6,11 @@
     <link rel="stylesheet" href="../css/default.css">
     <script src="../js/slideshow.js" defer></script>
     <link rel="stylesheet" href="../css/slideshow.css">
-
     <!--If defer is not used, the JavaScript function will run before everything
     is loaded and this will in turn make the slideshow not work as intended.
     It will not load the first Image properly. By using defer it loads all the HTML
     and then executes the Javascript so everything works as intended-->
+    <?php require_once "../php/database_connection.php"; ?>
 </head>
 <body>
 
@@ -40,7 +40,31 @@
     </div>
 </div>
 
+
+<?php
+// setting connection to the database
+$sql = "SELECT * FROM products;";
+$res=mysqli_query($connectAniverse, $sql);
+// fetches all rows
+while($row=mysqli_fetch_array($res))
+// if row is fetched while code is executed
+{
+?>
+
     <?php require "footer.php"; ?>
+
+<div class="product-grid" id="<?php echo $row["product_name"]; ?>">
+    <!-- html and php used together so that products from the data base are displayed -->
+    <img src="<?php echo $row["product_image"]; ?>" alt="" width=10% height=10% class="product-img" />
+    <!-- image source is fetched from the database, the location of the file is fetched and put inside the quotation marks -->
+    <h2 class="product-h2">£<?php echo $row["product_price"]; ?></h2>
+    <!-- product price is grabbed from the column -->
+    <p class="product-p"><?php echo $row["product_name"]; ?></p>
+    <!-- as well as the product name, for now I am not displaying the product description but just the information required for the display -->
+</div>
+<?php
+}
+?>
 
 </body>
 </html>
