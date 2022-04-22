7579042d17f52f61bc5d71de053b8379bcf6687f
Sameer Uddin <su042002@outlook.com>
2022-04-22 05:08:44 +0100

    Fixed footer colour problem
    Previously, when the footer was resized the colour would not increase with the footer so there would be a transparent background halfway through. This has been update and now the background stays dark blue every time the user resizes the window.

--- html/admin.php
+++ html/admin.php
@@ -56,7 +56,6 @@
         </div>
     </div>
 
-    <?php require "footer.php"; ?>
 
 </body>
 </html>
--- html/index.php
+++ html/index.php
@@ -5,12 +5,14 @@
     <title>Aniverse</title>
     <link rel="stylesheet" href="../css/default.css">
     <script src="../js/slideshow.js" defer></script>
+    <!--If defer is not used, the JavaScript function will run before everything
+is loaded and this will in turn make the slideshow not work as intended.
+It will not load the first Image properly. By using defer it loads all the HTML
+and then executes the Javascript so everything works as intended-->
     <link rel="stylesheet" href="../css/slideshow.css">
     <link rel="stylesheet" href="../css/main%20footer.css">
-    <!--If defer is not used, the JavaScript function will run before everything
-    is loaded and this will in turn make the slideshow not work as intended.
-    It will not load the first Image properly. By using defer it loads all the HTML
-    and then executes the Javascript so everything works as intended-->
+    <link rel="stylesheet" href="../css/products%20display.css">
+
     <?php require_once "../php/database_connection.php"; ?>
 </head>
 <body>
@@ -46,28 +48,29 @@
         <!--SLIDESHOW-->
 
         <!--PRODUCTS-->
-
-        <?php
-        // setting connection to the database
-        $sql = "SELECT * FROM products;";
-        $res = mysqli_query($connectAniverse, $sql);
-        // fetches all rows
-        while ($row = mysqli_fetch_array($res)) // if row is fetched while code is executed
-        {
-            ?>
-
-            <div class="products_display" id="<?php echo $row["product_name"]; ?>">
-                <!-- html and php used together so that products from the data base are displayed -->
-                <img src="<?php echo $row["product_image"]; ?>" alt="" width="150px" height="150px" class="product-img"/>
-                <!-- image source is fetched from the database, the location of the file is fetched and put inside the quotation marks -->
-                <h2 class="product-h2">£<?php echo $row["product_price"]; ?></h2>
-                <!-- product price is grabbed from the column -->
-                <p class="product-p"><?php echo $row["product_name"]; ?></p>
-                <!-- as well as the product name, for now I am not displaying the product description but just the information required for the display -->
-            </div>
+        <div class="products_gallery">
             <?php
-        }
-        ?>
+            // setting connection to the database
+            $sql = "SELECT * FROM products;";
+            $res = mysqli_query($connectAniverse, $sql);
+            // fetches all rows
+            while ($row = mysqli_fetch_array($res)) // if row is fetched while code is executed
+            {
+                ?>
+                <div class="content" id="<?php echo $row["product_name"]; ?>">
+                    <!-- html and php used together so that products from the data base are displayed -->
+                    <img src="<?php echo $row["product_image"]; ?>" alt="<?php echo $row["product_name"]; ?>" width="150px" height="150px" class="product-img"/>
+                    <!-- image source is fetched from the database, the location of the file is fetched and put inside the quotation marks -->
+                    <h2 class="product-h2">£<?php echo $row["product_price"]; ?></h2>
+                    <!-- product price is grabbed from the column -->
+                    <p class="product-p"><?php echo $row["product_name"]; ?></p>
+                    <!-- as well as the product name, for now I am not displaying the product description but just the information required for the display -->
+
+                </div>
+                <?php
+            }
+            ?>
+        </div>
     </div>
 </div>
 
