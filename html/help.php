406e37fcf475148846e46342d9fee6c82d8fdaf0
Sameer Uddin <su042002@outlook.com>
2022-04-28 00:22:16 +0100

    Added all contact on help page.

--- html/help.php
+++ html/help.php
@@ -15,10 +15,85 @@ and then executes the Javascript so everything works as intended-->
 <body>
 <?php require "header.php"; ?>
 
+<!--ALL THIS DUMMY TEXT WAS GENERATED FROM Lorem Ipsum-->
+
+<h2>If you have any issues contact:</h2>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Contact</th>
+    </tr>
+    <tr>
+        <td>Sameer Uddin</td>
+        <td>Chief executive officer</td>
+        <td><p><a href="mailto:sameer@email.com">Send email</a></p></td>
+    </tr>
+    <tr>
+        <td>Joe Mama</td>
+        <td>Human resources manager</td>
+        <td><p><a href="mailto:joe@email.com">Send email</a></p></td>
+    </tr>
+    <tr>
+        <td>Chris Biden</td>
+        <td>Information technology manager</td>
+        <td><p><a href="mailto:chris@email.com">Send email</a></p></td>
+    </tr>
+    <tr>
+        <td>Money Mark</td>
+        <td>Marketing manager</td>
+        <td><p><a href="mailto:money@email.com">Send email</a></p></td>
+    </tr>
+    <tr>
+        <td>Markass Brownlee</td>
+        <td>Product manager</td>
+        <td><p><a href="mailto:brownlee@email.com">Send email</a></p></td>
+    </tr>
+    <tr>
+        <td>Naruto Uzumaki</td>
+        <td>Sales manager</td>
+        <td><p><a href="mailto:naruto@email.com">Send email</a></p></td>
+    </tr>
+</table>
+
 <h2>Frequently Asked Questions</h2>
-<button type="button" class="section">Open Collapsible</button>
-<div class="content">
-    <p>Lorem ipsum he...</p>
+<button type="button" class="section">Where is my order?</button>
+<div class="information">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed lacus blandit, interdum orci ac, commodo arcu.
+        Donec ac nulla nec velit vestibulum auctor ullamcorper in neque. Nulla vel enim nulla. Vivamus convallis nibh in
+        varius interdum. Quisque eget mi a lacus iaculis hendrerit. Fusce nec congue ligula. Sed eget dapibus augue.
+        Praesent dolor nisl, imperdiet vitae justo vel, interdum placerat ligula. Etiam suscipit congue felis, vitae
+        pretium ipsum semper eget. Nam sit amet bibendum purus. Sed imperdiet turpis eget sapien hendrerit dictum. Ut
+        consectetur sagittis ex eget blandit.</p>
+    <br>
+    <h3>Check with these services to find your order:</h3>
+    <ul>
+        <li>Royal Mail</li>
+        <li>Hermes</li>
+        <li>DPD</li>
+    </ul>
+    <br>
+    <h3>If they were not helpful, follow these steps:</h3>
+    <ol type="a">
+        <li>Contact Customer Service</li>
+        <li>Drink Tea</li>
+        <li>Have a Red Bull</li>
+    </ol>
+</div>
+
+<button type="button" class="section">What will happen to my gift card?</button>
+<div class="information">
+    <p>Vestibulum finibus convallis tincidunt. Cras laoreet tempus pretium. Aliquam ante libero, semper nec porttitor
+        eget, pretium vehicula neque. Vestibulum et suscipit turpis, non mattis tortor. Nunc eget turpis finibus,
+        bibendum mi nec, euismod leo. Praesent at tincidunt turpis, sit amet porttitor purus. Proin eget odio non libero
+        egestas viverra. Ut dapibus nisi a quam vestibulum, et volutpat ex rhoncus. Ut pretium dictum aliquam. Sed
+        iaculis lobortis urna ac convallis. Aenean aliquam venenatis libero vitae ullamcorper. Nulla facilisi. Phasellus
+        sem lorem, blandit ut tincidunt id, convallis sit amet lorem. Sed fermentum imperdiet nulla eget ornare. Quisque
+        rutrum mauris in vulputate mattis.</p>
+</div>
+
+<button type="button" class="section">What will happen to my gift card?</button>
+<div class="information">
 </div>
 
 <?php require "footer.php"; ?>
