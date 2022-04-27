9761543d78e0c79f63c6193bc98eaef2f98c7dd6
Sameer Uddin <su042002@outlook.com>
2022-04-28 00:42:51 +0100

    Added all three sections to help page

--- html/help.php
+++ html/help.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Aniverse</title>
     <link rel="stylesheet" href="../css/default.css">
-    <link rel="stylesheet" href="../css/second%20footer.css">
+    <link rel="stylesheet" href="../css/main%20footer.css">
     <link rel="stylesheet" href="../css/help.css">
     <script src="../js/help.js" defer></script>
     <!--If defer is not used, the JavaScript function will run before everything
@@ -17,7 +17,7 @@ and then executes the Javascript so everything works as intended-->
 
 <!--ALL THIS DUMMY TEXT WAS GENERATED FROM Lorem Ipsum-->
 
-<h2>If you have any issues contact:</h2>
+<h2>IF YOU HAVE ISSUES YOU MAY CONTACT:</h2>
 <table>
     <tr>
         <th>Name</th>
@@ -56,7 +56,7 @@ and then executes the Javascript so everything works as intended-->
     </tr>
 </table>
 
-<h2>Frequently Asked Questions</h2>
+<h2>FREQUENTLY ASKED QUESTIONS</h2>
 <button type="button" class="section">Where is my order?</button>
 <div class="information">
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed lacus blandit, interdum orci ac, commodo arcu.
@@ -92,8 +92,196 @@ and then executes the Javascript so everything works as intended-->
         rutrum mauris in vulputate mattis.</p>
 </div>
 
-<button type="button" class="section">What will happen to my gift card?</button>
+<button type="button" class="section">When will my order arrive?</button>
 <div class="information">
+    <p> It will not arrive. Vestibulum diam magna, consequat posuere tempor et, euismod vitae nulla. Aenean tincidunt
+        dolor finibus erat semper, ac efficitur felis placerat. Vivamus non tortor dignissim, tristique neque id,
+        sagittis sem. Aliquam erat volutpat. Duis pretium sem sit amet nibh placerat, at finibus sem tincidunt. Integer
+        condimentum ligula quis massa dapibus tempus.
+
+        <br><br>
+
+        Nulla hendrerit magna risus, nec euismod lectus molestie id.
+        Aenean sodales venenatis nisl eget egestas. Aenean eu odio sit amet velit faucibus cursus. In vitae consectetur
+        libero, vitae imperdiet est. Nam sit amet scelerisque lorem, a porta magna. Proin non dui faucibus, ullamcorper
+        elit in, viverra velit. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus
+        et ultrices posuere cubilia curae; Donec tincidunt placerat orci, sed vestibulum est viverra et.</p>
+</div>
+
+<button type="button" class="section">How long do orders take to get fulfilled?</button>
+<div class="information">
+    <p>Sed vitae sem sed mauris feugiat placerat ut nec lectus. Nam non aliquam libero, et ultricies mauris. Aliquam
+        pellentesque nisi molestie dolor vestibulum, sed pharetra mauris finibus. Phasellus varius ultricies elit vitae
+        vestibulum. Nulla posuere semper lorem sed rutrum. Cras odio turpis, convallis sit amet porttitor at, vulputate
+        vel lorem. Vestibulum ullamcorper sem nisi, vel tincidunt arcu ultricies sed.</p>
+</div>
+
+<button type="button" class="section">How do I recieve my benefits?</button>
+<div class="information">
+    <p>Vestibulum id eleifend ex. Phasellus et metus magna. Suspendisse sed magna ut eros lobortis tempor nec at metus.
+        Etiam sed est gravida, ornare ex eget, ornare metus. Duis volutpat dolor ut sollicitudin gravida. Maecenas
+        rhoncus enim erat, sit amet volutpat arcu posuere vel. Curabitur est lacus, lacinia sit amet porttitor ut,
+        tempus a purus. Etiam tellus tellus, faucibus et aliquet in, cursus ac metus. Sed ac ligula lobortis, tempor ex
+        sed, condimentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae;<br><br>
+        Nunc vitae accumsan eros, ut ullamcorper dolor. Vestibulum vitae rhoncus ex, vitae imperdiet turpis. Quisque
+        commodo pretium sem, in suscipit turpis tempor eu. Quisque aliquam sed diam ut cursus. Quisque luctus vehicula
+        tristique. Curabitur mollis pellentesque magna. Curabitur vel risus fermentum, bibendum tellus quis, lobortis
+        ipsum. In hac habitasse platea dictumst. Maecenas dictum turpis ligula, ut lobortis magna dignissim vel. Nullam
+        aliquet neque at enim tristique aliquet. Fusce ut dui quis mi euismod bibendum. Suspendisse potenti. Aliquam non
+        quam cursus, sagittis turpis sit amet, ultricies risus.</p>
+</div>
+
+<h2>SHIPPING</h2>
+<button type="button" class="section">How much does shipping cost?</button>
+<div class="information">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed lacus blandit, interdum orci ac, commodo arcu.
+        Donec ac nulla nec velit vestibulum auctor ullamcorper in neque. Nulla vel enim nulla. Vivamus convallis nibh in
+        varius interdum. Quisque eget mi a lacus iaculis hendrerit. Fusce nec congue ligula. Sed eget dapibus augue.
+        Praesent dolor nisl, imperdiet vitae justo vel, interdum placerat ligula. Etiam suscipit congue felis, vitae
+        pretium ipsum semper eget. Nam sit amet bibendum purus. Sed imperdiet turpis eget sapien hendrerit dictum. Ut
+        consectetur sagittis ex eget blandit.</p>
+    <br>
+    <h3>Check with these services to find your order:</h3>
+    <ul>
+        <li>Royal Mail</li>
+        <li>Hermes</li>
+        <li>DPD</li>
+    </ul>
+    <br>
+    <h3>If they were not helpful, follow these steps:</h3>
+    <ol type="a">
+        <li>Contact Customer Service</li>
+        <li>Drink Tea</li>
+        <li>Have a Red Bull</li>
+    </ol>
+</div>
+
+<button type="button" class="section">What happens if my package is lost in transit?</button>
+<div class="information">
+    <p>Vestibulum finibus convallis tincidunt. Cras laoreet tempus pretium. Aliquam ante libero, semper nec porttitor
+        eget, pretium vehicula neque. Vestibulum et suscipit turpis, non mattis tortor. Nunc eget turpis finibus,
+        bibendum mi nec, euismod leo. Praesent at tincidunt turpis, sit amet porttitor purus. Proin eget odio non libero
+        egestas viverra. Ut dapibus nisi a quam vestibulum, et volutpat ex rhoncus. Ut pretium dictum aliquam. Sed
+        iaculis lobortis urna ac convallis. Aenean aliquam venenatis libero vitae ullamcorper. Nulla facilisi. Phasellus
+        sem lorem, blandit ut tincidunt id, convallis sit amet lorem. Sed fermentum imperdiet nulla eget ornare. Quisque
+        rutrum mauris in vulputate mattis.</p>
+</div>
+
+<button type="button" class="section">Do you ship abroad?</button>
+<div class="information">
+    <p> It will not arrive. Vestibulum diam magna, consequat posuere tempor et, euismod vitae nulla. Aenean tincidunt
+        dolor finibus erat semper, ac efficitur felis placerat. Vivamus non tortor dignissim, tristique neque id,
+        sagittis sem. Aliquam erat volutpat. Duis pretium sem sit amet nibh placerat, at finibus sem tincidunt. Integer
+        condimentum ligula quis massa dapibus tempus.
+
+        <br><br>
+
+        Nulla hendrerit magna risus, nec euismod lectus molestie id.
+        Aenean sodales venenatis nisl eget egestas. Aenean eu odio sit amet velit faucibus cursus. In vitae consectetur
+        libero, vitae imperdiet est. Nam sit amet scelerisque lorem, a porta magna. Proin non dui faucibus, ullamcorper
+        elit in, viverra velit. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus
+        et ultrices posuere cubilia curae; Donec tincidunt placerat orci, sed vestibulum est viverra et.</p>
+</div>
+
+<button type="button" class="section">When will my order be shipped?</button>
+<div class="information">
+    <p>Sed vitae sem sed mauris feugiat placerat ut nec lectus. Nam non aliquam libero, et ultricies mauris. Aliquam
+        pellentesque nisi molestie dolor vestibulum, sed pharetra mauris finibus. Phasellus varius ultricies elit vitae
+        vestibulum. Nulla posuere semper lorem sed rutrum. Cras odio turpis, convallis sit amet porttitor at, vulputate
+        vel lorem. Vestibulum ullamcorper sem nisi, vel tincidunt arcu ultricies sed.</p>
+</div>
+
+<button type="button" class="section">How do I qualify for free shipping?</button>
+<div class="information">
+    <p>Vestibulum id eleifend ex. Phasellus et metus magna. Suspendisse sed magna ut eros lobortis tempor nec at metus.
+        Etiam sed est gravida, ornare ex eget, ornare metus. Duis volutpat dolor ut sollicitudin gravida. Maecenas
+        rhoncus enim erat, sit amet volutpat arcu posuere vel. Curabitur est lacus, lacinia sit amet porttitor ut,
+        tempus a purus. Etiam tellus tellus, faucibus et aliquet in, cursus ac metus. Sed ac ligula lobortis, tempor ex
+        sed, condimentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae;<br><br>
+        Nunc vitae accumsan eros, ut ullamcorper dolor. Vestibulum vitae rhoncus ex, vitae imperdiet turpis. Quisque
+        commodo pretium sem, in suscipit turpis tempor eu. Quisque aliquam sed diam ut cursus. Quisque luctus vehicula
+        tristique. Curabitur mollis pellentesque magna. Curabitur vel risus fermentum, bibendum tellus quis, lobortis
+        ipsum. In hac habitasse platea dictumst. Maecenas dictum turpis ligula, ut lobortis magna dignissim vel. Nullam
+        aliquet neque at enim tristique aliquet. Fusce ut dui quis mi euismod bibendum. Suspendisse potenti. Aliquam non
+        quam cursus, sagittis turpis sit amet, ultricies risus.</p>
+</div>
+
+<h2>RETURNS POLICY</h2>
+<button type="button" class="section">What is your return policy?</button>
+<div class="information">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed lacus blandit, interdum orci ac, commodo arcu.
+        Donec ac nulla nec velit vestibulum auctor ullamcorper in neque. Nulla vel enim nulla. Vivamus convallis nibh in
+        varius interdum. Quisque eget mi a lacus iaculis hendrerit. Fusce nec congue ligula. Sed eget dapibus augue.
+        Praesent dolor nisl, imperdiet vitae justo vel, interdum placerat ligula. Etiam suscipit congue felis, vitae
+        pretium ipsum semper eget. Nam sit amet bibendum purus. Sed imperdiet turpis eget sapien hendrerit dictum. Ut
+        consectetur sagittis ex eget blandit.</p>
+    <br>
+    <h3>Check with these services to find your order:</h3>
+    <ul>
+        <li>Royal Mail</li>
+        <li>Hermes</li>
+        <li>DPD</li>
+    </ul>
+    <br>
+    <h3>If they were not helpful, follow these steps:</h3>
+    <ol type="a">
+        <li>Contact Customer Service</li>
+        <li>Drink Tea</li>
+        <li>Have a Red Bull</li>
+    </ol>
+</div>
+
+<button type="button" class="section">How do I return a product?</button>
+<div class="information">
+    <p>Vestibulum finibus convallis tincidunt. Cras laoreet tempus pretium. Aliquam ante libero, semper nec porttitor
+        eget, pretium vehicula neque. Vestibulum et suscipit turpis, non mattis tortor. Nunc eget turpis finibus,
+        bibendum mi nec, euismod leo. Praesent at tincidunt turpis, sit amet porttitor purus. Proin eget odio non libero
+        egestas viverra. Ut dapibus nisi a quam vestibulum, et volutpat ex rhoncus. Ut pretium dictum aliquam. Sed
+        iaculis lobortis urna ac convallis. Aenean aliquam venenatis libero vitae ullamcorper. Nulla facilisi. Phasellus
+        sem lorem, blandit ut tincidunt id, convallis sit amet lorem. Sed fermentum imperdiet nulla eget ornare. Quisque
+        rutrum mauris in vulputate mattis.</p>
+</div>
+
+<button type="button" class="section">What products are excluded in your returns policy?</button>
+<div class="information">
+    <p> It will not arrive. Vestibulum diam magna, consequat posuere tempor et, euismod vitae nulla. Aenean tincidunt
+        dolor finibus erat semper, ac efficitur felis placerat. Vivamus non tortor dignissim, tristique neque id,
+        sagittis sem. Aliquam erat volutpat. Duis pretium sem sit amet nibh placerat, at finibus sem tincidunt. Integer
+        condimentum ligula quis massa dapibus tempus.
+
+        <br><br>
+
+        Nulla hendrerit magna risus, nec euismod lectus molestie id.
+        Aenean sodales venenatis nisl eget egestas. Aenean eu odio sit amet velit faucibus cursus. In vitae consectetur
+        libero, vitae imperdiet est. Nam sit amet scelerisque lorem, a porta magna. Proin non dui faucibus, ullamcorper
+        elit in, viverra velit. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus
+        et ultrices posuere cubilia curae; Donec tincidunt placerat orci, sed vestibulum est viverra et.</p>
+</div>
+
+<button type="button" class="section">My product is defective, what do I do?</button>
+<div class="information">
+    <p>Sed vitae sem sed mauris feugiat placerat ut nec lectus. Nam non aliquam libero, et ultricies mauris. Aliquam
+        pellentesque nisi molestie dolor vestibulum, sed pharetra mauris finibus. Phasellus varius ultricies elit vitae
+        vestibulum. Nulla posuere semper lorem sed rutrum. Cras odio turpis, convallis sit amet porttitor at, vulputate
+        vel lorem. Vestibulum ullamcorper sem nisi, vel tincidunt arcu ultricies sed.</p>
+</div>
+
+<button type="button" class="section">What is your return period?</button>
+<div class="information">
+    <p>Vestibulum id eleifend ex. Phasellus et metus magna. Suspendisse sed magna ut eros lobortis tempor nec at metus.
+        Etiam sed est gravida, ornare ex eget, ornare metus. Duis volutpat dolor ut sollicitudin gravida. Maecenas
+        rhoncus enim erat, sit amet volutpat arcu posuere vel. Curabitur est lacus, lacinia sit amet porttitor ut,
+        tempus a purus. Etiam tellus tellus, faucibus et aliquet in, cursus ac metus. Sed ac ligula lobortis, tempor ex
+        sed, condimentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae;<br><br>
+        Nunc vitae accumsan eros, ut ullamcorper dolor. Vestibulum vitae rhoncus ex, vitae imperdiet turpis. Quisque
+        commodo pretium sem, in suscipit turpis tempor eu. Quisque aliquam sed diam ut cursus. Quisque luctus vehicula
+        tristique. Curabitur mollis pellentesque magna. Curabitur vel risus fermentum, bibendum tellus quis, lobortis
+        ipsum. In hac habitasse platea dictumst. Maecenas dictum turpis ligula, ut lobortis magna dignissim vel. Nullam
+        aliquet neque at enim tristique aliquet. Fusce ut dui quis mi euismod bibendum. Suspendisse potenti. Aliquam non
+        quam cursus, sagittis turpis sit amet, ultricies risus.</p>
 </div>
 
 <?php require "footer.php"; ?>
