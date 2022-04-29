4f2051db39bb5a8f124804d2a0673eaed6320f6d
Sameer Uddin <su042002@outlook.com>
2022-04-29 19:10:27 +0100

    Added delete form

--- html/admin.php
+++ html/admin.php
@@ -16,7 +16,6 @@ if (isset($_SESSION["userType"])) {
     if ($_SESSION["userType"] == "User") {
         header("location: index.php?error=notAdmin");
     } else if ($_SESSION["userType"] == "Admin") {
-        header("location: index.php?error=none");
     }
 } else {
     header("location: index.php?error=notAdmin");
@@ -29,6 +28,49 @@ if (isset($_SESSION["userType"])) {
             <form name="addProd" action="../php/product_submission.php" method="post" enctype="multipart/form-data"
                   class="addForm"> <!-- created a form -->
                 <h1>Add Product</h1> <!-- header so the user knows they can add product -->
+                <!-- enctype is needed because there are files that are uploaded, images -->
+                <p>All the fields are required to submit!</p>
+                <div class="prodInput">
+                    <input class="input_box" type="text" name="productName" placeholder="Product Name" required
+                           autocomplete="off">
+                </div> <!-- creating seperate divisions for all the inputs -->
+                <!-- auto complete is switched off for everything so history isn't saved -->
+                <!-- required tag here so that nothing is left empty before data is sent off -->
+                <div class="prodInput">
+                    <input class="input_box" type="text" name="productPrice" placeholder="Price" required
+                           autocomplete="off">
+                </div> <!-- input for price -->
+                <div class="prodInput">
+                    <textarea name="productDesc" rows="4" cols="0" autocomplete="off" placeholder="Description"
+                              required></textarea>
+                </div> <!-- so that the user can write product description -->
+                <div class="prodImage">
+                    <label>
+                        Upload Image
+                        <input type="file" name="productImg" placeholder="Add Image" required autocomplete="off"
+                               style="visibility: hidden;">
+                    </label> <!-- label used so it can be customized in css -->
+                </div>
+                <div class="prodInput">
+                    <select name="productCat" required>
+                        <option value="DVD">DVD</option>
+                        <option value="Manga">Manga</option>
+                        <option value="Figures">Figures</option>
+                        <option value="Games">Games</option>
+                        <option value="Accessories">Accessories</option>
+                    </select> <!-- drop down menu so that the user can put items into
+      different categories -->
+                </div>
+
+                <div class="prodInput" id="submit_button">
+                    <button type="submit" class="add-button" name="submission">Add Product</button>
+                </div> <!-- when the user presses the button they will be able to submit the information -->
+
+            </form>
+
+            <form name="deleteProd" action="../php/product_submission.php" method="post" enctype="multipart/form-data"
+                  class="deleteForm"> <!-- created a form -->
+                <h1>Delete Product</h1> <!-- header so the user knows they can add product -->
                 <!-- enctype is needed because there are files that are uploaded, images -->
                 <p>All the fields are required to submit!</p>
                 <div class="prodInput">
