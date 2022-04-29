f6fd8a36012d2fdc42fe3faa9347af5ce2d702a7
Sameer Uddin <su042002@outlook.com>
2022-04-29 19:32:53 +0100

    Add Product and Delete Product positioned better

--- html/admin.php
+++ html/admin.php
@@ -77,33 +77,6 @@ if (isset($_SESSION["userType"])) {
                     <input class="input_box" type="text" name="productName" placeholder="Product Name" required
                            autocomplete="off">
                 </div> <!-- creating seperate divisions for all the inputs -->
-                <!-- auto complete is switched off for everything so history isn't saved -->
-                <!-- required tag here so that nothing is left empty before data is sent off -->
-                <div class="prodInput">
-                    <input class="input_box" type="text" name="productPrice" placeholder="Price" required
-                           autocomplete="off">
-                </div> <!-- input for price -->
-                <div class="prodInput">
-                    <textarea name="productDesc" rows="4" cols="0" autocomplete="off" placeholder="Description"
-                              required></textarea>
-                </div> <!-- so that the user can write product description -->
-                <div class="prodImage">
-                    <label>
-                        Upload Image
-                        <input type="file" name="productImg" placeholder="Add Image" required autocomplete="off"
-                               style="visibility: hidden;">
-                    </label> <!-- label used so it can be customized in css -->
-                </div>
-                <div class="prodInput">
-                    <select name="productCat" required>
-                        <option value="DVD">DVD</option>
-                        <option value="Manga">Manga</option>
-                        <option value="Figures">Figures</option>
-                        <option value="Games">Games</option>
-                        <option value="Accessories">Accessories</option>
-                    </select> <!-- drop down menu so that the user can put items into
-      different categories -->
-                </div>
 
                 <div class="prodInput" id="submit_button">
                     <button type="submit" class="add-button" name="submission">Add Product</button>
