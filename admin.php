8c702ab535e2f64556dd315712b8790ffa5f13a4
Sameer Uddin <su042002@outlook.com>
2022-04-30 19:50:57 +0100

    File Upload now works
    Fixed a bug where image was not uploading for local host.

--- admin.php
+++ admin.php
@@ -30,7 +30,7 @@ if (isset($_SESSION["userType"])) {
 <div id="container">
     <div id="main">
         <div class="product_container" id="all_products">
-            <form name="addProd" action="../php/product_submission.php" method="post" enctype="multipart/form-data"
+            <form name="addProd" action="php/product_submission.php" method="post" enctype="multipart/form-data"
                   class="addForm"> <!-- created a form -->
                 <h1>Add Product</h1> <!-- header so the user knows they can add product -->
                 <!-- enctype is needed because there are files that are uploaded, images -->
@@ -76,16 +76,16 @@ if (isset($_SESSION["userType"])) {
 
         <form name="addProd" action="php/product_submission.php" method="post" enctype="multipart/form-data"
               class="addForm">
-        <table>
-            <thead>
-            <tr>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Description</th>
-                <th>Upload Image</th>
-                <th>Category</th>
-            </tr>
-            </thead>
+            <table>
+                <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                    <th>Upload Image</th>
+                    <th>Category</th>
+                </tr>
+                </thead>
                 <tr>
                     <td>
                         <div class="prodInput">
@@ -100,10 +100,39 @@ if (isset($_SESSION["userType"])) {
                         </div>
                     </td>
                     <td>
-
+                        <div class="prodInput">
+                              <textarea name="productDesc" rows="4" cols="0" autocomplete="off" placeholder="Description"
+                              required></textarea>
+                        </div> <!-- so that the user can write product description -->
+                    </td>
+                    <td>
+                        <div class="prodImage">
+                            <label>
+                                Upload Image
+                                <input type="file" name="productImg" placeholder="Add Image" required autocomplete="off"
+                                       style="visibility: hidden;">
+                            </label> <!-- label used so it can be customized in css -->
+                        </div>
+                    </td>
+                    <td>
+                        <div class="prodInput" id="select_options">
+                            <select name="productCat" required>
+                                <option value="DVD">DVD</option>
+                                <option value="Manga">Manga</option>
+                                <option value="Figures">Figures</option>
+                                <option value="Games">Games</option>
+                                <option value="Accessories">Accessories</option>
+                            </select> <!-- drop down menu so that the user can put items into
+      different categories -->
+                        </div>
+                    </td>
+                    <td>
+                        <div class="prodInput" id="submit_button">
+                            <button type="submit" class="add-button" name="submission">Add Product</button>
+                        </div> <!-- when the user presses the button they will be able to submit the information -->
                     </td>
                 </tr>
-        </table>
+            </table>
         </form>
 
         <div>
@@ -115,28 +144,28 @@ if (isset($_SESSION["userType"])) {
                     <th>Category</th>
                 </tr>
                 </thead>
+                <?php
+                // setting connection to the database
+                $sql = "SELECT * FROM products;";
+                $res = mysqli_query($connectAniverse, $sql);
+                // fetches all rows
+                while ($row = mysqli_fetch_array($res)) // if row is fetched while code is executed
+                {
+                    ?>
+                    <tr>
+                        <td><?php echo $row["product_name"] ?></td>
+                        <td><?php echo $row["product_price"] ?></td>
+                        <td><?php echo $row["product_category"] ?></td>
+                        <td>
+                            <a class="edit_button" href="admin.php?edit=<?php echo $row["id"]; ?>">Edit</a>
+                        </td>
+                        <td>
+                            <a class="delete_button" href="admin.php?delete=<?php echo $row["id"]; ?>">Delete</a>
+                        </td>
+                    </tr>
                     <?php
-                    // setting connection to the database
-                    $sql = "SELECT * FROM products;";
-                    $res = mysqli_query($connectAniverse, $sql);
-                    // fetches all rows
-                    while ($row = mysqli_fetch_array($res)) // if row is fetched while code is executed
-                    {
-                    ?>
-                        <tr>
-                            <td><?php echo $row["product_name"]?></td>
-                            <td><?php echo $row["product_price"]?></td>
-                            <td><?php echo $row["product_category"]?></td>
-                            <td>
-                                <a class="edit_button" href="admin.php?edit=<?php echo $row["id"]; ?>">Edit</a>
-                            </td>
-                            <td>
-                                <a class="delete_button" href="admin.php?delete=<?php echo $row["id"]; ?>">Delete</a>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                    ?>
+                }
+                ?>
             </table>
         </div>
 
